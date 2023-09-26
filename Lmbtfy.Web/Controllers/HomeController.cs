67f8a18a7ee24f3684a1e47e4414b124f7d3a088
Steven Ryland <stevenry@outlook.com>
2023-09-26 09:52:53 -0700

    My customizations

--- Lmbtfy.Web/Controllers/HomeController.cs
+++ Lmbtfy.Web/Controllers/HomeController.cs
@@ -5,35 +5,48 @@ using System.Net;
 
 namespace Lmbtfy.Web.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
         public HomeController()
         {
-            
+
         }
 
+        [Route("[action]")]
         public ActionResult About()
         {
             return View();
         }
 
-        public ActionResult Index(string q)
-        {
-            if (string.IsNullOrEmpty(q))
-            {
-                return View();
-            }
-
-            ViewBag.Question = q;
-            return View("BingThis");
-        }
-
+        [Route("[action]")]
         public ActionResult GenerateUrl(string q)
         {
             string path = Url.ActionLink("Index", "Home", new { q }, "https", Request.Host.Value);
             string tinyUrl = GenerateTinyUrl(path);
 
             return PartialView("_GenerateUrl", new GeneratedUrlModel { Url = path, TinyUrl = tinyUrl });
+        }
+
+        [Route("[action]")]
+        [Route("/{**text}")]
+        public ActionResult Index(string text, string q)
+        {
+            if (string.IsNullOrEmpty(q))
+            {
+                if (!string.IsNullOrEmpty(text))
+                {
+                    text = text.Replace('+', ' ')
+                        .Replace('-', ' ')
+                        .Replace('/', ' ')
+                        .Replace('_', ' ');
+                    return Redirect($"/?q={text}");
+                }
+                return View();
+            }
+
+            ViewBag.Question = q;
+            return View("BingThis");
         }
 
         protected string GenerateTinyUrl(string realUrl)
